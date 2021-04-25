aaf2edaa124d232363216c11552df42fdbc98025
acparkgh <acpark123@gmail.com>
2021-04-25 19:36:42 -0400

    blog code

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const currentYear = 2021;
+
+function whatIsCurrentYear() {
+  
+  const currentMonth = "April";
+
+  function week() {
+    day();
+  };
+  const currentWeek = "first";
+  
+  function day() {
+    const currentDay = "25th";
+    console.log(
+      `We are in ${currentDay}, ${currentWeek} week of ${currentMonth}, ${currentYear}.`
+    );
+  };
+
+  week();
+  
+};
+
+whatIsCurrentYear();
+
