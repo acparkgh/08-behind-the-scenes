d6dbf1d0ef3482ed66174d0376cd81ab27f248e5
acparkgh <acpark123@gmail.com>
2021-04-26 02:19:58 -0400

    scope chain

--- starter/script.js
+++ starter/script.js
@@ -1,26 +1,34 @@
 'use strict';
 
-const currentYear = 2021;
-
-function whatIsCurrentYear() {
+function calcAge (birthYear) {
+  const age = 2037 - birthYear;
   
-  const currentMonth = "April";
+  function printAge() {
+    let output = `${firstName}, your are ${age}, born in ${birthYear}`;
+    console.log(output);
 
-  function week() {
-    day();
-  };
-  const currentWeek = "first";
-  
-  function day() {
-    const currentDay = "25th";
-    console.log(
-      `We are in ${currentDay}, ${currentWeek} week of ${currentMonth}, ${currentYear}.`
-    );
-  };
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;  //var is function scope
+      const firstName = "Steven";
+      
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+ 
+      function add(a, b) {
+        return a + b;
+      }
+      output = "New Output!!!"
+    }
+    // console.log(add(2, 3));
+    console.log(millenial) // millenial is var variable
+    console.log(output);
+      
+  }
+  printAge();
+    
+  return age;
+}
+const firstName = "Jonas";
+calcAge(1991);
 
-  week();
-  
-};
-
-whatIsCurrentYear();
 
